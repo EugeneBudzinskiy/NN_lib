121cbfd9f613059ce1d8878ac52e719f6d035fac
EugeneBudzinskiy <eugene.budzinskiy@gmail.com>
2021-06-01 13:01:23 +0300

    minor update

--- nnlibrary/__init__.py
+++ nnlibrary/__init__.py
@@ -1,6 +1,4 @@
 from nnlibrary import activation
-# from nnlibrary.constructor import constructor
-# from nnlibrary.layers import Layers
 from nnlibrary.losses import Losses
 from nnlibrary.optimizers import Optimizers
 from nnlibrary.singleton import SingletonMeta
