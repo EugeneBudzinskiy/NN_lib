Fixed `gradient` in forward and reverse mode. Extended functionality of variables and operations for them.

nnlibrary/auto_diff/forward_mode/math_ops.py
@@ -58,7 +58,7 @@ class Power(ForwardBiOperation):
     @staticmethod
     def call(x1: AbstractSpecialVariable, x2: AbstractSpecialVariable) -> AbstractSpecialVariable:
         value = x1.value ** x2.value
-        log_x1 = np.log(Power.epsilon) if abs(x1.value) < Power.epsilon else np.log(x1.value)
+        log_x1 = np.log(Power.epsilon) if abs(x1.value) < Power.epsilon else np.log(abs(x1.value))
         partial = x1.value ** (x2.value - 1) * (x1.partial * x2.value + x1.value * x2.partial * log_x1)
         return special_vars.Variable(value=value, partial=partial)
 

nnlibrary/auto_diff/reverse_mode/math_ops.py
@@ -57,7 +57,7 @@ class Division(ReverseBiOperation):
 class Power(ReverseBiOperation):
     @staticmethod
     def get_inputs_partials(x1: AbstractSpecialVariable, x2: AbstractSpecialVariable) -> tuple:
-        log_x1 = np.log(Power.epsilon) if abs(x1.value) < Power.epsilon else np.log(x1.value)
+        log_x1 = np.log(Power.epsilon) if abs(x1.value) < Power.epsilon else np.log(abs(x1.value))
         return x1.value ** (x2.value - 1) * x2.value, x1.value ** x2.value * log_x1
 
     @staticmethod
@@ -106,7 +106,7 @@ class SquareRoot(ReverseUniOperation):
 class Exponent(ReverseUniOperation):
     @staticmethod
     def get_inputs_partials(x: AbstractSpecialVariable) -> tuple:
-        return np.sqrt(x.value),
+        return np.exp(x.value),
 
     @staticmethod
     def call(x: AbstractSpecialVariable) -> AbstractSpecialVariable:

nnlibrary/losses/losses.py
@@ -35,5 +35,5 @@ class CategoricalCrossentropy(AbstractLoss):
         y_predicted = np.maximum(y_predicted, self.epsilon)
         y_predicted = np.minimum(y_predicted, 1. - self.epsilon)
 
-        value = - np.sum(np.log(y_predicted) * y_target, axis=-1).reshape(1, -1)
+        value = - np.sum(y_target * np.log(y_predicted), axis=-1).reshape(1, -1)
         return reduction(values=value)