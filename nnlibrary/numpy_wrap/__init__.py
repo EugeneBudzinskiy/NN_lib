e649dd95d02d106d22f9f0a5d5dd2f112835d111
EugeneBudzinskiy <eugene.budzinskiy@gmail.com>
2022-09-04 13:21:03 +0300

    Added rest functionality `array_creation` package from `numpy` to `numpy_wrap`

--- nnlibrary/numpy_wrap/__init__.py
+++ nnlibrary/numpy_wrap/__init__.py
@@ -10,6 +10,20 @@ from .array_creation import zeros
 from .array_creation import zeros_like
 from .array_creation import full
 from .array_creation import full_like
+from .array_creation import array
+from .array_creation import asarray
+from .array_creation import copy
+from .array_creation import arange
+from .array_creation import linspace
+from .array_creation import logspace
+from .array_creation import geomspace
+from .array_creation import meshgrid
+from .array_creation import diag
+from .array_creation import diagflat
+from .array_creation import tri
+from .array_creation import triu
+from .array_creation import tril
+from .array_creation import vander
 
 
-ndarray = numpy.ndarray
+ndarray = numpy.ndarray  # Variable for typing numpy arrays
--- nnlibrary/numpy_wrap/array_creation.py
+++ nnlibrary/numpy_wrap/array_creation.py
@@ -17,12 +17,14 @@ def empty(shape: Union[int, Iterable, tuple[int]],
     return Node(values=npw.numpy.empty(shape=shape, dtype=dtype, order=order, *args, **kwargs))
 
 
-def empty_like(prototype: Union[npw.ndarray, Iterable, int, float],
+def empty_like(prototype: Union[npw.ndarray, Iterable, int, float, AbstractNode],
                dtype: Optional[object] = None,
                order: Optional[str] = None,
                subok: Optional[bool] = None,
                shape: Union[int, Iterable[int], None] = None) -> AbstractNode:
-    return Node(values=npw.numpy.empty_like(prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
+    prototype = prototype.values if isinstance(prototype, AbstractNode) else prototype
+    return Node(values=npw.numpy.empty_like(
+        prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
 
 
 # noinspection PyPep8Naming
@@ -51,12 +53,14 @@ def ones(shape: Union[int, Iterable, tuple[int]],
     return Node(values=npw.numpy.ones(shape=shape, dtype=dtype, order=order, *args, **kwargs))
 
 
-def ones_like(prototype: Union[npw.ndarray, Iterable, int, float],
+def ones_like(prototype: Union[npw.ndarray, Iterable, int, float, AbstractNode],
               dtype: Optional[object] = None,
               order: Optional[str] = None,
               subok: Optional[bool] = None,
               shape: Union[int, Iterable[int], None] = None) -> AbstractNode:
-    return Node(values=npw.numpy.ones_like(prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
+    prototype = prototype.values if isinstance(prototype, AbstractNode) else prototype
+    return Node(values=npw.numpy.ones_like(
+        prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
 
 
 def zeros(shape: Union[int, Iterable, tuple[int]],
@@ -67,12 +71,14 @@ def zeros(shape: Union[int, Iterable, tuple[int]],
     return Node(values=npw.numpy.zeros(shape=shape, dtype=dtype, order=order, *args, **kwargs))
 
 
-def zeros_like(prototype: Union[npw.ndarray, Iterable, int, float],
+def zeros_like(prototype: Union[npw.ndarray, Iterable, int, float, AbstractNode],
                dtype: Optional[object] = None,
                order: Optional[str] = None,
                subok: Optional[bool] = None,
                shape: Union[int, Iterable[int], None] = None) -> AbstractNode:
-    return Node(values=npw.numpy.zeros_like(prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
+    prototype = prototype.values if isinstance(prototype, AbstractNode) else prototype
+    return Node(values=npw.numpy.zeros_like(
+        prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
 
 
 def full(shape: Union[int, Iterable, tuple[int]],
@@ -83,10 +89,129 @@ def full(shape: Union[int, Iterable, tuple[int]],
     return Node(values=npw.numpy.full(shape=shape, dtype=dtype, order=order, *args, **kwargs))
 
 
-def full_like(prototype: Union[npw.ndarray, Iterable, int, float],
+def full_like(prototype: Union[npw.ndarray, Iterable, int, float, AbstractNode],
               dtype: Optional[object] = None,
               order: Optional[str] = None,
               subok: Optional[bool] = None,
               shape: Union[int, Iterable[int], None] = None) -> AbstractNode:
-    return Node(values=npw.numpy.full_like(prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
+    prototype = prototype.values if isinstance(prototype, AbstractNode) else prototype
+    return Node(values=npw.numpy.full_like(
+        prototype=prototype, dtype=dtype, order=order, subok=subok, shape=shape))
 
+
+def array(p_object: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+          dtype: Optional[object] = None,
+          *args: Any,
+          **kwargs: Any) -> AbstractNode:
+    p_object = p_object.values if isinstance(p_object, AbstractNode) else p_object
+    return Node(values=npw.numpy.array(p_object=p_object, dtype=dtype, *args, **kwargs))
+
+
+def asarray(a: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+            dtype: Optional[object] = None,
+            order: Optional[str] = None,
+            *args: Any,
+            **kwargs: Any) -> AbstractNode:
+    a = a.values if isinstance(a, AbstractNode) else a
+    return Node(values=npw.numpy.asarray(a=a, dtype=dtype, order=order, *args, **kwargs))
+
+
+def copy(a: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+         order: Optional[str] = 'K',
+         subok: Optional[bool] = False) -> AbstractNode:
+    a = a.values if isinstance(a, AbstractNode) else a
+    return Node(values=npw.numpy.copy(a=a, order=order, subok=subok))
+
+
+def arange(start: Optional[int] = None, *args: Any, **kwargs: Any) -> AbstractNode:
+    return Node(values=npw.numpy.arange(start=start, *args, **kwargs))
+
+
+def linspace(start: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+             stop: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+             num: Optional[int] = 50,
+             endpoint: Optional[bool] = True,
+             retstep: Optional[bool] = False,
+             dtype: Optional[object] = None,
+             axis: Optional[int] = 0) -> Any:
+    start = start.values if isinstance(start, AbstractNode) else start
+    stop = stop.values if isinstance(stop, AbstractNode) else stop
+    result = npw.numpy.linspace(
+        start=start, stop=stop, num=num, endpoint=endpoint, retstep=retstep, dtype=dtype, axis=axis)
+    return (Node(values=result[0]), result[1]) if retstep else Node(values=result)
+
+
+def logspace(start: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+             stop: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+             num: Optional[int] = 50,
+             endpoint: Optional[bool] = True,
+             base: Union[npw.ndarray, Iterable, int, float, None] = 10.0,
+             dtype: object = None,
+             axis: Optional[int] = 0) -> AbstractNode:
+    start = start.values if isinstance(start, AbstractNode) else start
+    stop = stop.values if isinstance(stop, AbstractNode) else stop
+    return Node(values=npw.numpy.logspace(
+        start=start, stop=stop, num=num, endpoint=endpoint, base=base, dtype=dtype, axis=axis))
+
+
+def geomspace(start: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+              stop: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+              num: Optional[int] = 50,
+              endpoint: Optional[bool] = True,
+              dtype: object = None,
+              axis: Optional[int] = 0) -> AbstractNode:
+    start = start.values if isinstance(start, AbstractNode) else start
+    stop = stop.values if isinstance(stop, AbstractNode) else stop
+    return Node(values=npw.numpy.geomspace(
+        start=start, stop=stop, num=num, endpoint=endpoint, dtype=dtype, axis=axis))
+
+
+# noinspection PyShadowingNames
+def meshgrid(*xi: Any,
+             copy: Optional[bool] = True,
+             sparse: Optional[bool] = False,
+             indexing: Optional[str] = 'xy') -> AbstractNode:
+    xi = tuple([x.values if isinstance(x, AbstractNode) else x for x in xi])
+    return Node(values=npw.numpy.meshgrid(*xi, copy=copy, sparse=sparse, indexing=indexing))
+
+
+def diag(v: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+         k: Optional[int] = 0) -> AbstractNode:
+    v = v.values if isinstance(v, AbstractNode) else v
+    return Node(values=npw.numpy.diag(v=v, k=k))
+
+
+def diagflat(v: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+             k: Optional[int] = 0) -> AbstractNode:
+    v = v.values if isinstance(v, AbstractNode) else v
+    return Node(values=npw.numpy.diagflat(v=v, k=k))
+
+
+# noinspection PyPep8Naming
+def tri(N: int,
+        M: Optional[int] = None,
+        k: Optional[int] = 0,
+        dtype: Optional[object] = float,
+        *args: Any,
+        **kwargs: Any) -> AbstractNode:
+    return Node(values=npw.numpy.tri(N=N, M=M, k=k, dtype=dtype, *args, **kwargs))
+
+
+def tril(m: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+         k: Optional[int] = 0) -> AbstractNode:
+    m = m.values if isinstance(m, AbstractNode) else m
+    return Node(values=npw.numpy.tril(m=m, k=k))
+
+
+def triu(m: Union[npw.ndarray, Iterable, int, float, AbstractNode],
+         k: Optional[int] = 0) -> AbstractNode:
+    m = m.values if isinstance(m, AbstractNode) else m
+    return Node(values=npw.numpy.triu(m=m, k=k))
+
+
+# noinspection PyPep8Naming
+def vander(x: Union[npw.ndarray, Iterable, int, float],
+           N: Optional[int] = None,
+           increasing: Optional[bool] = False) -> AbstractNode:
+    x = x.values if isinstance(x, AbstractNode) else x
+    return Node(values=npw.numpy.vander(x=x, N=N, increasing=increasing))
